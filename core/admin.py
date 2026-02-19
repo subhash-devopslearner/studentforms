4a2218996e59f30ee9f3a1daef0bdb2a38624db1
Subhash Bhalerao <subhash.devops.learner@gmail.com>
2026-02-19 23:51:20 +0530

    Add core models in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import AcademicYear, Department, Program, Branch, Semester
 
 # Register your models here.
+admin.site.register(AcademicYear)
+admin.site.register(Department)
+admin.site.register(Program)
+admin.site.register(Branch)
+admin.site.register(Semester)
